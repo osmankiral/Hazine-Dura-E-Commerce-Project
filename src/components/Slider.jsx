9c7b6ff2766ab1fe59c1b80642b3ca4c11e8574e
osmankiral <osman.kiral96@gmail.com>
2024-02-03 18:25:11 +0300

    added slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "../styles/swiperStyles.css";
+import Banner1 from '../public/banner1.png'
+import Banner2 from '../public/banner2.png'
+import Banner3 from '../public/banner3.png'
+
+const Slider = () => {
+  return (
+    <div>
+      <Swiper className="mySwiper">
+        <SwiperSlide>
+            <img  src={Banner1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Banner2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Banner3} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Header from "../components/Header";
 import BestSeller from '../components/BestSeller';
 import Products from '../components/Products';
 import Footer from "../components/Footer";
+import Slider from '../components/Slider';
 
 
 const Home = () => {
   return (
     <>
         <Header/>
+        <Slider/>
         <BestSeller/>
         <Products/>
         <Footer/>
